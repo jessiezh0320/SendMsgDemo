7961d9722d42104d467265f0a2e67ff9c348582a
Jessie <michelle3208@163.com>
2019-07-09 16:50:54 +0800

    the first commit

---
+++ App24/App24/MainActivity.cs
@@ -0,0 +1,209 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content.PM;
+using Android.Telephony;
+using System;
+using Android;
+using Android.Content;
+using Android.Support.V4.Content;
+using Android.Support.V4.App;
+using Android.Util;
+using Android.Support.Design.Widget;
+using Android.Views;
+
+namespace App24
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, ActivityCompat.IOnRequestPermissionsResultCallback 
+    {
+        static readonly int REQUEST_SENDSMS = 0;
+        static readonly int REQUEST_READ_PHONE_STATE = 0x001;
+
+
+
+        View layout;
+        private SmsManager _smsManager;
+        private BroadcastReceiver _smsSentBroadcastReceiver, _smsDeliveredBroadcastReceiver;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.layout1);
+            layout = FindViewById(Resource.Id.sample_main_layout);
+            Button smsBtn = FindViewById<Button>(Resource.Id.btnSend);
+            EditText phoneNum = FindViewById<EditText>(Resource.Id.phoneNum);
+            EditText sms = FindViewById<EditText>(Resource.Id.txtSMS);
+            _smsManager = SmsManager.Default;
+
+            smsBtn.Click += (s, e) =>
+            {
+                var phone = phoneNum.Text;
+                var message = sms.Text;
+                var piSent = PendingIntent.GetBroadcast(this, 0, new Intent("SMS_SENT"), 0);
+                var piDelivered = PendingIntent.GetBroadcast(this, 0, new Intent("SMS_DELIVERED"), 0);
+
+                if ((int)Build.VERSION.SdkInt < 23)
+                {
+                    _smsManager.SendTextMessage(phone, null, message, piSent, piDelivered);
+                    return;
+                }
+                else
+                {
+                    if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.SendSms) != (int)Permission.Granted)
+                    {
+                        // Permission is not granted. If necessary display rationale & request.
+                        RequestSendSMSPermission();
+                    }
+                    else
+                    {
+                        //_smsManager.SendTextMessage(phone, null, message, piSent, piDelivered);
+
+                        if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.ReadPhoneState) != (int)Permission.Granted)
+                        {
+                            // Permission is not granted. If necessary display rationale & request.
+                            RequestReadPhoneStatePermission();
+                        }
+                        else
+                        {
+                            // We have permission, go ahead and send SMS.
+                            _smsManager.SendTextMessage(phone, null, message, piSent, piDelivered);
+
+                        }
+
+
+
+                    }
+                }
+            };
+        }
+
+        private void RequestReadPhoneStatePermission()
+        {
+            Log.Info("MainActivity", "READ_PHONE_STATE permission has NOT been granted. Requesting permission.");
+
+            if (ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.ReadPhoneState))
+            {
+                // Provide an additional rationale to the user if the permission was not granted
+                // and the user would benefit from additional context for the use of the permission.
+                // For example if the user has previously denied the permission.
+                Log.Info("MainActivity", " READ_PHONE_STATE permission rationale to provide additional context.");
+                //Activity activity = CrossCurrentActivity.Current.Activity;
+                //Android.Views.View activityRootView = activity.FindViewById(Android.Resource.Id.Content);
+                Snackbar.Make(layout, "READ_PHONE_STATE permission is needed to send SMS.",
+                    Snackbar.LengthIndefinite).SetAction("OK", new Action<View>(delegate (View obj) {
+                        ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.ReadPhoneState }, REQUEST_READ_PHONE_STATE);
+                    })).Show();
+            }
+            else
+            {
+                // Message permission has not been granted yet. Request it directly.
+                ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.ReadPhoneState }, REQUEST_READ_PHONE_STATE);
+            }
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            _smsSentBroadcastReceiver = new SMSSentReceiver();
+            _smsDeliveredBroadcastReceiver = new SMSDeliveredReceiver();
+
+            RegisterReceiver(_smsSentBroadcastReceiver, new IntentFilter("SMS_SENT"));
+            RegisterReceiver(_smsDeliveredBroadcastReceiver, new IntentFilter("SMS_DELIVERED"));
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+
+            UnregisterReceiver(_smsSentBroadcastReceiver);
+            UnregisterReceiver(_smsDeliveredBroadcastReceiver);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+        void RequestSendSMSPermission()
+        {
+            Log.Info("MainActivity", "Message permission has NOT been granted. Requesting permission.");
+
+            if (ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.SendSms))
+            {
+                // Provide an additional rationale to the user if the permission was not granted
+                // and the user would benefit from additional context for the use of the permission.
+                // For example if the user has previously denied the permission.
+                Log.Info("MainActivity", "Displaying message permission rationale to provide additional context.");
+                //Activity activity = CrossCurrentActivity.Current.Activity;
+                //Android.Views.View activityRootView = activity.FindViewById(Android.Resource.Id.Content);
+                Snackbar.Make(layout, "Message permission is needed to send SMS.",
+                    Snackbar.LengthIndefinite).SetAction("OK", new Action<View>(delegate (View obj) {
+                        ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.SendSms }, REQUEST_SENDSMS);
+                    })).Show();
+            }
+            else
+            {
+                // Message permission has not been granted yet. Request it directly.
+                ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.SendSms }, REQUEST_SENDSMS);
+            }
+        }
+    }
+
+
+
+
+
+
+
+    [BroadcastReceiver]
+    public class SMSSentReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            switch ((int)ResultCode)
+            {
+                case (int)Result.Ok:
+                    Toast.MakeText(Application.Context, "SMS has been sent", ToastLength.Short).Show();
+                    break;
+                case (int)SmsResultError.GenericFailure:
+                    Toast.MakeText(Application.Context, "Generic Failure", ToastLength.Short).Show();
+                    break;
+                case (int)SmsResultError.NoService:
+                    Toast.MakeText(Application.Context, "No Service", ToastLength.Short).Show();
+                    break;
+                case (int)SmsResultError.NullPdu:
+                    Toast.MakeText(Application.Context, "Null PDU", ToastLength.Short).Show();
+                    break;
+                case (int)SmsResultError.RadioOff:
+                    Toast.MakeText(Application.Context, "Radio Off", ToastLength.Short).Show();
+                    break;
+            }
+        }
+    }
+
+    [BroadcastReceiver]
+    public class SMSDeliveredReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            switch ((int)ResultCode)
+            {
+                case (int)Result.Ok:
+                    Toast.MakeText(Application.Context, "SMS Delivered", ToastLength.Short).Show();
+                    break;
+                case (int)Result.Canceled:
+                    Toast.MakeText(Application.Context, "SMS not delivered", ToastLength.Short).Show();
+                    break;
+            }
+        }           
+
+        
+    }
+}
